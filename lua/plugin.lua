14da2b36a0657b27f458426d9151fb0389a60cc0
kiridore <ashenone.jiang@gmail.com>
2024-06-03 18:09:24 +0800

    删除会导致lsp错误的dim插件

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -177,30 +177,6 @@ require("lazy").setup({
         "j-hui/fidget.nvim",
         opts = { },
     },
-    -- 基于LSP显示未使用的局部变量
-    {
-        "zbirenbaum/neodim",
-        event = "LspAttach",
-        config = function()
-            require("neodim").setup({
-                refresh_delay = 75,
-                alpha = 0.75,
-                blend_color = "#000000",
-                hide = {
-                    underline = true,
-                    virtual_text = true,
-                    signs = true,
-                },
-                regex = {
-                    "[uU]nused",
-                    "[nN]ever [rR]ead",
-                    "[nN]ot [rR]ead",
-                },
-                priority = 128,
-                disable = {},
-            })
-        end
-    }
 })
 
 -- 启动插件
