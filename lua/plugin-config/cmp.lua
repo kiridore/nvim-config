12d4b8830064c76a540615d7e1b16b42fbbebcfa
kiridore <ashenone.jiang@gmail.com>
2024-06-11 16:47:52 +0800

    爆改cmp，修复在c++文件夹下补全列表不对齐的问题

--- lua/plugin-config/cmp.lua
+++ lua/plugin-config/cmp.lua
@@ -1,20 +1,35 @@
+local border = function(hl)
+    return {
+        { "┌", hl },
+        { "─", hl },
+        { "┐", hl },
+        { "│", hl },
+        { "┘", hl },
+        { "─", hl },
+        { "└", hl },
+        { "│", hl },
+    }
+end
+
 -- Set up nvim-cmp.
 local cmp = require'cmp'
-
 cmp.setup({
     snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
             require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-            -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
         end,
     },
     window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
+        completion = {
+            border = border("PmenuBorder"),
+            winhighlight = "Normal:Pmenu,CursorLine:PmenuSel,Search:PmenuSel",
+            scrollbar = true,
+        },
+        documentation = {
+            border = border("CmpDocBorder"),
+            winhighlight = "Normal:CmpDoc",
+        },
     },
     mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -24,14 +39,70 @@ cmp.setup({
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        -- { name = 'vsnip' }, -- For vsnip users.
+        { name = 'nvim_lsp', max_item_count = 350 },
         { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
-    }, {
-            { name = 'buffer' },
-        })
+        { name = 'treesitter' },
+        {
+            name = 'buffer',
+            option = {
+                get_bufnrs = function ()
+                    return vim.api.nvim_list_bufs()
+                end,
+            }
+        },
+        { name = 'path' },
+    }),
+    experimental = {
+        ghost_text = {
+            hl_group = "Whitespace",
+        }
+    },
+    performance = {
+        async_budget = 1,
+        max_view_entries = 120,
+    },
+    matching = {
+        disallow_partial_fuzzy_matching = false,
+    },
+    formatting = {
+        fields = { "abbr", "kind", "menu" },
+        format = function(entry, vim_item)
+            -- load lspkind icons
+            vim_item.kind =
+            string.format("%s", vim_item.kind or "")
+
+            vim_item.menu = setmetatable({
+                cmp_tabnine = "[TN]",
+                copilot = "[CPLT]",
+                buffer = "[BUF]",
+                orgmode = "[ORG]",
+                nvim_lsp = "[LSP]",
+                nvim_lua = "[LUA]",
+                path = "[PATH]",
+                tmux = "[TMUX]",
+                treesitter = "[TS]",
+                latex_symbols = "[LTEX]",
+                luasnip = "[SNIP]",
+                spell = "[SPELL]",
+                }, {
+                    __index = function()
+                        return "[BTN]" -- builtin/unknown source names
+                    end,
+            })[entry.source.name]
+
+            local label = vim_item.abbr
+            local max_length = 60
+            local lable_without_space = string.gsub(label, "^%s+", "")
+            local truncated_label = vim.fn.strcharpart(lable_without_space, 0, max_length)
+            if truncated_label ~= lable_without_space then
+                vim_item.abbr = truncated_label .. "..."
+            else
+                vim_item.abbr = truncated_label
+            end
+
+            return vim_item
+        end,
+    },
 })
 
 -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
@@ -65,7 +136,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['clangd'].setup {
     capabilities = capabilities
