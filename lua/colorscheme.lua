52be0d8a600700f21c38c0fa9fcc08a57cf17da6
kiridore <ashenone.jiang@gmail.com>
2025-05-07 23:59:12 +0800

    关闭neovide的阴影

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,3 +1,3 @@
 vim.o.background = "dark"
 
-vim.cmd([[colorscheme catppuccin]])
+vim.cmd([[colorscheme everforest]])
--- lua/neovide-config.lua
+++ lua/neovide-config.lua
@@ -5,4 +5,6 @@ if vim.g.neovide then
     vim.g.neovide_scroll_animation_length = 0.1
     -- 键入时隐藏鼠标
     vim.g.neovide_hide_mouse_when_typing = true
+    -- 关闭漂浮阴影
+    vim.g.neovide_floating_shadow = false
 end
