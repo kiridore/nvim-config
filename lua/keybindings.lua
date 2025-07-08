d8fd7005c931bce3b930e2cf4cba41906ce18d43
kiridore <ashenone.jiang@gmail.com>
2025-07-08 23:38:00 +0800

    格式化代码

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -124,18 +124,19 @@ map("v", "<leader>c", "<Cmd>HopChar1MW<CR>", opt)
 map("v", "<leader>C", "<Cmd>HopChar2MW<CR>", opt)
 
 -- dap
-vim.keymap.set("n", "<F6>", function () require("dap").continue() end, opt)
-vim.keymap.set("n", "<F7>", function () require("dap").terminate() end, opt)
-vim.keymap.set("n", "<F8>", function () require("dap").toggle_breakpoint() end, opt)
-vim.keymap.set("n", "<F9>", function () require("dap").step_into() end, opt)
-vim.keymap.set("n", "<F10>", function () require("dap").step_out() end, opt)
-vim.keymap.set("n", "<F11>", function () require("dap").step_over() end, opt)
+vim.keymap.set("n", "<F6>", function() require("dap").continue() end, opt)
+vim.keymap.set("n", "<F7>", function() require("dap").terminate() end, opt)
+vim.keymap.set("n", "<F8>", function() require("dap").toggle_breakpoint() end, opt)
+vim.keymap.set("n", "<F9>", function() require("dap").step_into() end, opt)
+vim.keymap.set("n", "<F10>", function() require("dap").step_out() end, opt)
+vim.keymap.set("n", "<F11>", function() require("dap").step_over() end, opt)
 
 
-vim.keymap.set("n", "<leader>db", function () require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, opt)
-vim.keymap.set("n", "<leader>dc", function () require("dap").run_to_cursor() end, opt)
-vim.keymap.set("n", "<leader>dl", function () require("dap").run_last() end, opt)
-vim.keymap.set("n", "<leader>do", function () require("dap").open() end, opt)   -- open repl
+vim.keymap.set("n", "<leader>db", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end,
+    opt)
+vim.keymap.set("n", "<leader>dc", function() require("dap").run_to_cursor() end, opt)
+vim.keymap.set("n", "<leader>dl", function() require("dap").run_last() end, opt)
+vim.keymap.set("n", "<leader>do", function() require("dap").open() end, opt) -- open repl
 
 -- clever-f
 map("n", ";", "<Plug>(clever-f-repeat-forward)", opt)
