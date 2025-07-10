ac4a73069d48d0f68d9d724c04690a39256aa03e
kiridore <ashenone.jiang@gmail.com>
2025-07-10 23:53:37 +0800

    添加clangd的特例设置，确保找到电脑中的c++编译器

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -3,6 +3,15 @@ local mason_lspconfig = require("mason-lspconfig")
 
 mason_lspconfig.setup_handlers({
     function(server_name) -- 默认处理
-        lspconfig[server_name].setup({})
+        if server_name == "clangd" then
+            lspconfig[server_name].setup({
+                cmd = {
+                    "clangd",
+                    "--query-driver=C:/msys64/mingw64/bin/*"
+                }
+            })
+        else
+            lspconfig[server_name].setup({})
+        end
     end,
 })
