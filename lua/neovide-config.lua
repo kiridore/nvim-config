b1b670faa398c636c1b12681777b79450a32a1b9
kiridore <ashenone.jiang@gmail.com>
2026-04-29 05:19:55 +0800

    feat(neovide): 更新neovide配置

--- lua/neovide-config.lua
+++ lua/neovide-config.lua
@@ -2,9 +2,14 @@ if vim.g.neovide then
     vim.o.guifont = 'FiraMono Nerd Font Mono:h13'
     -- 动画速度加快
     vim.g.neovide_cursor_animation_length = 0.03
-    vim.g.neovide_scroll_animation_length = 0.1
+    vim.g.neovide_scroll_animation_length = 0.08
+    vim.g.neovide_scroll_animation_far_lines = 1
     -- 键入时隐藏鼠标
     vim.g.neovide_hide_mouse_when_typing = true
     -- 关闭漂浮阴影
     vim.g.neovide_floating_shadow = false
+    vim.g.neovide_remember_window_size = true
+    vim.g.neovide_refresh_rate = 144
+
+    vim.g.neovide_cursor_vfx_mode = ""
 end
