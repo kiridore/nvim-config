8fa6415d76de7ce17f02733eff3a3acf944c237d
kiridore <ashenone.jiang@gmail.com>
2026-05-04 03:50:33 +0800

    feat(key): 重新整理按键绑定

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -18,6 +18,8 @@ local opt = {
 -- 代理函数
 local map = vim.api.nvim_set_keymap
 
+----------基础编辑---------
+
 -- 复制粘贴
 map("n", "<leader>y", "\"+y", opt)
 map("n", "<leader>p", "\"+p", opt)
@@ -28,6 +30,22 @@ map("v", "<leader>p", "\"+p", opt)
 -- 搜索时填充very magic标识
 map("n", "/", "/\\v", opt)
 map("v", "/", "/\\v", opt)
+
+-- 删除buffers
+map("n", "<A-q>", "<Cmd>BufDel<CR>", opt)
+
+-- 加速jk
+map('n', 'j', '<Plug>(accelerated_jk_gj)', opt)
+map('n', 'k', '<Plug>(accelerated_jk_gk)', opt)
+
+-- clever-f
+map("n", ";", "<Plug>(clever-f-repeat-forward)", opt)
+map("n", ",", "<Plug>(clever-f-repeat-back)", opt)
+
+-- format
+map("n", "<leader>=", ":lua vim.lsp.buf.format { async = true }<CR>", opt)
+
+----------窗口与导航---------
 
 -- alt + hjkl  窗口之间跳转
 map("n", "<C-h>", "<C-w>h", opt)
@@ -41,16 +59,38 @@ map("n", "<C-Right>", ":vertical resize +2<CR>", opt)
 map("n", "<C-Down>", ":resize +2<CR>", opt)
 map("n", "<C-Up>", ":resize -2<CR>", opt)
 
-----------plugin---------
+-- hop better motion
+map("n", "<leader>w", "<Cmd>HopWordMW<CR>", opt)
+map("n", "<leader>j", "<Cmd>HopLineMW<CR>", opt)
+map("n", "<leader>k", "<Cmd>HopLineMW<CR>", opt)
 
---- nvim-tree
+map("v", "<leader>w", "<Cmd>HopWordMW<CR>", opt)
+map("v", "<leader>j", "<Cmd>HopLineMW<CR>", opt)
+map("v", "<leader>k", "<Cmd>HopLineMW<CR>", opt)
+map("v", "<leader>c", "<Cmd>HopChar1MW<CR>", opt)
+map("v", "<leader>C", "<Cmd>HopChar2MW<CR>", opt)
+
+----------终端---------
+
+-- terminal
+map("t", "<ESC><ESC>", "<C-\\><C-n>", opt)
+map("n", "<C-\\>", "<Cmd>ToggleTerm direction=horizontal<CR>", opt)
+map("i", "<C-\\>", "<ESC><Cmd>ToggleTerm direction=horizontal<CR>", opt)
+
+----------插件：nvim-tree---------
+
 map("n", "<leader>m", ":NvimTreeToggle<CR>", opt)
 
---- telescope
+----------插件：telescope---------
+
 map('n', '<leader>e', ":Telescope find_files<CR>", opt)
 map('n', '<leader>fg', ":Telescope live_grep<CR>", opt)
 map('n', '<leader>fb', ":Telescope buffers<CR>", opt)
 map('n', '<leader>fh', ":Telescope help_tags<CR>", opt)
+
+vim.keymap.set("n", "<leader>gm", require("telescope.builtin").marks, opt)
+
+----------插件：LSP (Lspsaga + Glance)---------
 
 map("n", "<leader>r", "<Cmd>Lspsaga rename<CR>", opt)
 map("n", "ga", "<Cmd>Lspsaga code_action<CR>", opt)
@@ -62,54 +102,14 @@ map("n", "gci", "<Cmd>Lspsaga incoming_calls<CR>", opt)
 map("n", "gco", "<Cmd>Lspsaga outgoing_calls<CR>", opt)
 map("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opt)
 
--- format
-map("n", "<leader>=", ":lua vim.lsp.buf.format { async = true }<CR>", opt)
+----------插件：trouble---------
 
--- terminal
-map("t", "<ESC><ESC>", "<C-\\><C-n>", opt)
-map("n", "<C-\\>", "<Cmd>ToggleTerm direction=horizontal<CR>", opt)
-map("i", "<C-\\>", "<ESC><Cmd>ToggleTerm direction=horizontal<CR>", opt)
-
--- trouble
 map("n", "<leader>xx", "<Cmd>Trouble diagnostics toggle <CR>", opt)
 map("n", "<leader>xl", "<Cmd>Trouble lsp toggle win.position=right<CR>", opt)
 map("n", "<leader>xs", "<Cmd>Trouble lsp_document_symbols toggle win.position=right<CR>", opt)
 
--- bufferline
--- map("n", "<A-o>", ":BufferLineCycleNext<CR>", opt)
--- map("n", "<A-i>", ":BufferLineCyclePrev<CR>", opt)
---
--- map("n", "<A-1>", ":BufferLineGoToBuffer 1<CR>", opt)
--- map("n", "<A-2>", ":BufferLineGoToBuffer 2<CR>", opt)
--- map("n", "<A-3>", ":BufferLineGoToBuffer 3<CR>", opt)
--- map("n", "<A-4>", ":BufferLineGoToBuffer 4<CR>", opt)
--- map("n", "<A-5>", ":BufferLineGoToBuffer 5<CR>", opt)
--- map("n", "<A-6>", ":BufferLineGoToBuffer 6<CR>", opt)
--- map("n", "<A-7>", ":BufferLineGoToBuffer 7<CR>", opt)
--- map("n", "<A-8>", ":BufferLineGoToBuffer 8<CR>", opt)
--- map("n", "<A-9>", ":BufferLineGoToBuffer 9<CR>", opt)
+----------插件：dap (调试)---------
 
--- delete buffers
-map("n", "<A-q>", "<Cmd>BufDel<CR>", opt)
-
--- 加速jk
-map('n', 'j', '<Plug>(accelerated_jk_gj)', opt)
-map('n', 'k', '<Plug>(accelerated_jk_gk)', opt)
-
--- hop better motion
-map("n", "<leader>w", "<Cmd>HopWordMW<CR>", opt)
-map("n", "<leader>j", "<Cmd>HopLineMW<CR>", opt)
-map("n", "<leader>k", "<Cmd>HopLineMW<CR>", opt)
--- map("n", "<leader>c", "<Cmd>HopChar1MW<CR>", opt)
--- map("n", "<leader>C", "<Cmd>HopChar2MW<CR>", opt)
-
-map("v", "<leader>w", "<Cmd>HopWordMW<CR>", opt)
-map("v", "<leader>j", "<Cmd>HopLineMW<CR>", opt)
-map("v", "<leader>k", "<Cmd>HopLineMW<CR>", opt)
-map("v", "<leader>c", "<Cmd>HopChar1MW<CR>", opt)
-map("v", "<leader>C", "<Cmd>HopChar2MW<CR>", opt)
-
--- dap
 vim.keymap.set("n", "<F6>", function() require("dap").continue() end, opt)
 vim.keymap.set("n", "<F7>", function() require("dap").terminate() end, opt)
 vim.keymap.set("n", "<F8>", function() require("dap").toggle_breakpoint() end, opt)
@@ -117,21 +117,13 @@ vim.keymap.set("n", "<F9>", function() require("dap").step_into() end, opt)
 vim.keymap.set("n", "<F10>", function() require("dap").step_out() end, opt)
 vim.keymap.set("n", "<F11>", function() require("dap").step_over() end, opt)
 
-
 vim.keymap.set("n", "<leader>db", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end,
     opt)
 vim.keymap.set("n", "<leader>dc", function() require("dap").run_to_cursor() end, opt)
 vim.keymap.set("n", "<leader>dl", function() require("dap").run_last() end, opt)
-vim.keymap.set("n", "<leader>do", function() require("dap").open() end, opt) -- open repl
+vim.keymap.set("n", "<leader>do", function() require("dap").open() end, opt)
 
--- clever-f
-map("n", ";", "<Plug>(clever-f-repeat-forward)", opt)
-map("n", ",", "<Plug>(clever-f-repeat-back)", opt)
-
--- dap-ui
--- map("n", "<A-k>", "<Cmd>lua require(\"dapui\").eval()<CR>", opt)
-
-vim.keymap.set("n", "<leader>gm", require("telescope.builtin").marks, opt)
+----------插件：crates (Rust)---------
 
 local crates = require("crates")
 local opts = { silent = true }
@@ -160,18 +152,17 @@ vim.keymap.set("n", "<leader>cD", crates.open_documentation, opts)
 vim.keymap.set("n", "<leader>cC", crates.open_crates_io, opts)
 vim.keymap.set("n", "<leader>cL", crates.open_lib_rs, opts)
 
--- CodeCompanion
-vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "<leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
-vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+----------插件：fugitive (Git)---------
 
--- Expand 'cc' into 'CodeCompanion' in the command line
-vim.cmd([[cab cc CodeCompanion]])
-
--- fugitive
 vim.keymap.set("n", "<leader>gs", "<cmd>Git<CR>", { desc = "Git Status" })
 vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<CR>", { desc = "Git Commit" })
 vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>", { desc = "Git Push" })
 vim.keymap.set("n", "<leader>gl", "<cmd>Git pull<CR>", { desc = "Git Pull" })
 vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<CR>", { desc = "Git Blame" })
 vim.keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<CR>")
+
+----------插件：CodeCompanion---------
+
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd([[cab cc CodeCompanion]])
+
